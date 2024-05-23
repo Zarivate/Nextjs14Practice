c203df286d92170834a359e9305eb462d2fb110a
Zarivate <Zarivate@gmail.com>
2024-05-23 03:02:59 -0500

    Setting up Clerk, among routes and authentication

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return <div>ProfilePage goes here</div>;
+};
+
+export default ProfilePage;
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// This file will handle showing all the details of any alterations done by the website
+const TransformationDetails = () => {
+  return <div>TransformationDetails</div>;
+};
+
+export default TransformationDetails;
