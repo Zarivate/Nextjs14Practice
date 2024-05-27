08451cd34bd70db921a5652a56e541da68163066
Zarivate <Zarivate@gmail.com>
2024-05-27 03:52:37 -0500

    Got MongoDB cluster working, compatible with Clerk and Vercel now.

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,15 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+// In order to adjust what gets displayed according to what page the user heads to, the [type] field in the folder structure is destructured and displayed
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  // Get the corresponding url header type by comparing and finding it to the ones listed in the constants
+  const transformation = transformationTypes[type];
+
+  return (
+    <Header title={transformation.title} subtitle={transformation.subTitle} />
+  );
 };
 
 export default AddTransformationTypePage;
