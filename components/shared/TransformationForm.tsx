f1ad9408bb70fac5f0c278b0ab5ff8722005e468
Zarivate <Zarivate@gmail.com>
2024-05-29 23:59:25 -0500

    Working on user input forms still

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -13,11 +13,25 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { Input } from "@/components/ui/input";
-import { defaultValues } from "@/constants";
+import {
+  aspectRatioOptions,
+  defaultValues,
+  transformationTypes,
+} from "@/constants";
+import { CustomField } from "./CustomField";
+import { AspectRatioKey } from "@/lib/utils";
 
 // Handles the types of  input fields and their validation
-const formSchema = z.object({
+export const formSchema = z.object({
   title: z.string(),
   aspectRation: z.string().optional(),
   color: z.string().optional(),
@@ -28,7 +42,20 @@ const formSchema = z.object({
 const TransformationForm = ({
   action,
   data = null,
+  userId,
+  type,
+  creditBalance,
 }: TransformationFormProps) => {
+  // Grab the type of image alteration a user wants to do
+  const transformationType = transformationTypes[type];
+
+  // TODO: Implement proper image uploading. For now just store the image data
+  const [image, setImage] = useState(data);
+
+  // Store the image transformation
+  const [newTransformation, setNewTransformation] =
+    useState<Transformations | null>(null);
+
   // To account for the possibility of someone editing an image instead of creating a new one, checks to see if there is any exisiting data first
   // before grabbing the data fields.
   const initialValues =
@@ -50,16 +77,95 @@ const TransformationForm = ({
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
   }
 
+  const onSelectFieldHandler = (
+    value: string,
+    onChangeField: (value: string) => void
+  ) => {
+    console.log("This has to be implemented still, silly");
+  };
+
+  // Handles user attempting to change an image
+  const onInputChangeHandler = (
+    fieldName: string,
+    value: string,
+    type: string,
+    onChangeField: (value: string) => void
+  ) => {
+    console.log(fieldName);
+  };
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <Button type="submit">Submit</Button>
-      </form>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8"></form>
+      {/* All the passed in props get passed to the render field and spread out, proper assignment of values is handled by the CustomField component */}
+      <CustomField
+        control={form.control}
+        name="title"
+        formLabel="Image Title"
+        className="w-full"
+        render={({ field }) => <Input {...field} className="input-field" />}
+      />
+
+      {/* The drop down menu only appears if the user is on the "fill" page */}
+      {type === "fill" && (
+        <CustomField
+          control={form.control}
+          name="aspectRatio"
+          FormLabel="Aspect Ratio"
+          className="w-full"
+          render={({ field }) => (
+            //
+            <Select
+              onValueChange={(value) =>
+                onSelectFieldHandler(value, field.onChange)
+              }
+            >
+              <SelectTrigger className="select-field">
+                <SelectValue placeholder="Select size" />
+              </SelectTrigger>
+              <SelectContent>
+                {/* Dynamically import the aspect ratio options, map over them and pass them to the Select Item component */}
+                {Object.keys(aspectRatioOptions).map((key) => (
+                  <SelectItem key={key} value={key} className="select-item">
+                    {aspectRatioOptions[key as AspectRatioKey].label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          )}
+        />
+      )}
+
+      {/* Handle the case where the user is on the remove or recolor option page */}
+      {(type === "remove" || type === "recolor") && (
+        <div className="prompt-field">
+          <CustomField
+            control={form.control}
+            name="prompt"
+            formLabel={
+              type === "remove" ? "object to remove" : "Object to recolor"
+            }
+            className="w-full"
+            render={({ field }) => (
+              <Input
+                value={field.value}
+                className="input-field"
+                onChange={(e) =>
+                  onInputChangeHandler(
+                    "prompt",
+                    e.target.value,
+                    type,
+                    field.onChange
+                  )
+                }
+              />
+            )}
+          />
+        </div>
+      )}
     </Form>
   );
 };
